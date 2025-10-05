d503c4058e2843f0fc27626f913943f40f1a84ca
PoeticSwam <alex.suesstrunk@gmail.com>
2025-10-05 12:41:06 +0200

    Initial commit

---
+++ src/main/java/net/poeticswam/minusmod/MinusMod.java
@@ -0,0 +1,16 @@
+package net.poeticswam.minusmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinusMod implements ModInitializer {
+	public static final String MOD_ID = "minusmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+    }
+}
