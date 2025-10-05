784e4fbd6a89122a047f7390d0ecd2a30c94dea4
PoeticSwam <alex.suesstrunk@gmail.com>
2025-10-05 16:10:37 +0200

    creeper crunch ice cream

--- src/main/java/net/poeticswam/minusmod/item/ModItems.java
+++ src/main/java/net/poeticswam/minusmod/item/ModItems.java
@@ -1,14 +1,18 @@
 package net.poeticswam.minusmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.poeticswam.minusmod.MinusMod;
 
 public class ModItems {
 
-    public static final Item
+    public static final Item CREEPER_CRUNCH_ICE_CREAM = registerItem("creeper_crunch_ice_cream", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MinusMod.MOD_ID,"creeper_crunch_ice_cream")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(MinusMod.MOD_ID, name), item);
@@ -16,5 +20,10 @@ public class ModItems {
 
     public static void registerModItems() {
         MinusMod.LOGGER.info("Registering Mod Items for" + MinusMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(CREEPER_CRUNCH_ICE_CREAM);
+        });
+
     }
 }
