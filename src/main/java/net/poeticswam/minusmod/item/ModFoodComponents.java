db840232ec840d995c4d41742cfb44b5985522fc
PoeticSwam <alex.suesstrunk@gmail.com>
2025-10-12 00:49:35 +0200

    ghast tear swirl ice cream

---
+++ src/main/java/net/poeticswam/minusmod/item/ModFoodComponents.java
@@ -0,0 +1,8 @@
+package net.poeticswam.minusmod.item;
+
+import net.minecraft.component.type.FoodComponent;
+
+public class ModFoodComponents {
+    public static final FoodComponent CREEPER_CRUNCH_ICE_CREAM = new FoodComponent.Builder().nutrition(2).saturationModifier(0.2F).build();
+    public static final FoodComponent GHAST_TEAR_SWIRL_ICE_CREAM = new FoodComponent.Builder().nutrition(2).saturationModifier(0.2F).build();
+}
--- src/main/java/net/poeticswam/minusmod/item/ModItems.java
+++ src/main/java/net/poeticswam/minusmod/item/ModItems.java
@@ -12,7 +12,8 @@ import net.poeticswam.minusmod.MinusMod;
 
 public class ModItems {
 
-    public static final Item CREEPER_CRUNCH_ICE_CREAM = registerItem("creeper_crunch_ice_cream", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MinusMod.MOD_ID,"creeper_crunch_ice_cream")))));
+    public static final Item CREEPER_CRUNCH_ICE_CREAM = registerItem("creeper_crunch_ice_cream", new Item(new Item.Settings().food(ModFoodComponents.CREEPER_CRUNCH_ICE_CREAM).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MinusMod.MOD_ID,"creeper_crunch_ice_cream")))));
+    public static final Item GHAST_TEAR_SWIRL_ICE_CREAM = registerItem("ghast_tear_swirl_ice_cream", new Item(new Item.Settings().food(ModFoodComponents.GHAST_TEAR_SWIRL_ICE_CREAM).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MinusMod.MOD_ID,"ghast_tear_swirl_ice_cream")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(MinusMod.MOD_ID, name), item);
