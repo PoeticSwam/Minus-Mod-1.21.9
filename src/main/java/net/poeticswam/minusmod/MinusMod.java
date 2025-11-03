a66bd2cca44c1c8500b1f668b35b48b556b9ff5e
PoeticSwam <alex.suesstrunk@gmail.com>
2025-11-03 10:50:25 +0100

    Cologne Potion (doesn't work yet)

--- src/main/java/net/poeticswam/minusmod/MinusMod.java
+++ src/main/java/net/poeticswam/minusmod/MinusMod.java
@@ -2,8 +2,13 @@ package net.poeticswam.minusmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potions;
 import net.poeticswam.minusmod.effect.ModEffects;
 import net.poeticswam.minusmod.item.ModItems;
+import net.poeticswam.minusmod.potion.ModPotions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,5 +20,9 @@ public class MinusMod implements ModInitializer {
 	public void onInitialize() {
         ModItems.registerModItems();
         ModEffects.registerEffects();
+        ModPotions.registerPotions();
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
+            builder.registerPotionRecipe(Potions.MUNDANE, Items.SLIME_BALL, ModPotions.MONSTER_MASTER_COLOGNE);
+        });
     }
 }
---
+++ src/main/java/net/poeticswam/minusmod/potion/ModPotions.java
@@ -0,0 +1,25 @@
+package net.poeticswam.minusmod.potion;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
+import net.poeticswam.minusmod.MinusMod;
+import net.poeticswam.minusmod.effect.ModEffects;
+
+public class ModPotions {
+
+    public static final RegistryEntry<Potion> MONSTER_MASTER_COLOGNE = registerPotion("monster_master_cologne",
+            new Potion(new StatusEffectInstance(ModEffects.COLOGNE, 1200, 0)));
+
+    private static RegistryEntry<Potion> registerPotion(String name, Potion potion) {
+        return Registry.registerReference(Registries.POTION, Identifier.of(MinusMod.MOD_ID, name), potion);
+    }
+
+    public static void registerPotions() {
+        MinusMod.LOGGER.info("Registering Mod Potions for" + MinusMod.MOD_ID);
+    }
+}
