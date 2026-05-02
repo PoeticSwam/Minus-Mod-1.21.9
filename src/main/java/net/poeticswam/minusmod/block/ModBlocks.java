591e84bec9af4d216ac4e4fac1629ff92402f662
PoeticSwam <alex.suesstrunk@gmail.com>
2026-05-02 22:38:54 +0200

    Present color models + crafting recipes

--- src/main/java/net/poeticswam/minusmod/block/ModBlocks.java
+++ src/main/java/net/poeticswam/minusmod/block/ModBlocks.java
@@ -40,6 +40,9 @@ public class ModBlocks {
     public static final Block PRESENT_AMETHYST_SHARD = registerCustomBlock("present_amethyst_shard", AbstractBlock.Settings.create().mapColor(MapColor.BLUE).strength(1F).sounds(BlockSoundGroup.WOOL).nonOpaque(), CustomPresentBlock::new);
     public static final Block PRESENT_ECHO_SHARD = registerCustomBlock("present_echo_shard", AbstractBlock.Settings.create().mapColor(MapColor.BLUE).strength(1F).sounds(BlockSoundGroup.WOOL).nonOpaque(), CustomPresentBlock::new);
     public static final Block PRESENT_BLUE_EMPTY = registerCustomBlock("present_blue_empty", AbstractBlock.Settings.create().mapColor(MapColor.BLUE).strength(1F).sounds(BlockSoundGroup.WOOL).nonOpaque(), CustomEmptyPresentBlock::new);
+    public static final Block PRESENT_GREEN_EMPTY = registerCustomBlock("present_green_empty", AbstractBlock.Settings.create().mapColor(MapColor.BLUE).strength(1F).sounds(BlockSoundGroup.WOOL).nonOpaque(), CustomEmptyPresentBlock::new);
+    public static final Block PRESENT_PINK_EMPTY = registerCustomBlock("present_pink_empty", AbstractBlock.Settings.create().mapColor(MapColor.BLUE).strength(1F).sounds(BlockSoundGroup.WOOL).nonOpaque(), CustomEmptyPresentBlock::new);
+    public static final Block PRESENT_RED_EMPTY = registerCustomBlock("present_red_empty", AbstractBlock.Settings.create().mapColor(MapColor.BLUE).strength(1F).sounds(BlockSoundGroup.WOOL).nonOpaque(), CustomEmptyPresentBlock::new);
 
     // --- Normal Blocks ---
     public static final Block EXAMPLE_BLOCK = registerBlock("example_block", AbstractBlock.Settings.create().mapColor(MapColor.PINK).instrument(NoteBlockInstrument.BASS).strength(1.5F).sounds(BlockSoundGroup.WOOL));
@@ -102,20 +105,26 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
             entries.add(SUGAR_BLOCK);
             entries.add(EXAMPLE_BLOCK);
-            entries.add(PRESENT_BLUE_EMPTY);
-            entries.add(PRESENT_IRON_INGOT);
-            entries.add(PRESENT_IRON_NUGGET);
-            entries.add(PRESENT_GOLD_INGOT);
-            entries.add(PRESENT_GOLD_NUGGET);
-            entries.add(PRESENT_COPPER_INGOT);
-            entries.add(PRESENT_COPPER_NUGGET);
-            entries.add(PRESENT_NETHERITE_INGOT);
-            entries.add(PRESENT_NETHERITE_SCRAP);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
             entries.add(PRESENT_LAPIS_LAZULI);
             entries.add(PRESENT_EMERALD);
             entries.add(PRESENT_DIAMOND);
+            entries.add(PRESENT_IRON_INGOT);
+            entries.add(PRESENT_GOLD_INGOT);
+            entries.add(PRESENT_COPPER_INGOT);
+            entries.add(PRESENT_NETHERITE_INGOT);
+            entries.add(PRESENT_IRON_NUGGET);
+            entries.add(PRESENT_GOLD_NUGGET);
+            entries.add(PRESENT_COPPER_NUGGET);
+            entries.add(PRESENT_NETHERITE_SCRAP);
             entries.add(PRESENT_AMETHYST_SHARD);
             entries.add(PRESENT_ECHO_SHARD);
+            entries.add(PRESENT_BLUE_EMPTY);
+            entries.add(PRESENT_RED_EMPTY);
+            entries.add(PRESENT_GREEN_EMPTY);
+            entries.add(PRESENT_PINK_EMPTY);
         });
     }
 }
